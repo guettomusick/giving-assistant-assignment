bc1d92b5cc7a7324087d51361e47b91a2d545337
Fernando Caamaño <guettomusick@gmail.com>
2020-08-06 14:41:49 -0300

    Change starpi config

--- cms/config/server.js
+++ cms/config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('URL', `http://localhost:${ env.int('PORT', 1337)}`),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '991f4b30a857d28ea11268eb94967129'),
